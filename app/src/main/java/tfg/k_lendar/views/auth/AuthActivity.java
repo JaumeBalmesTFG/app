fbb4aadd2d7e319c8139f4b8b66a0796a8e72b5a
Maik <mcollb20@jaumebalmes.net>
2022-03-17 18:08:27 +0100

    initial commit

---
+++ app/src/main/java/tfg/k_lendar/views/auth/AuthActivity.java
@@ -0,0 +1,16 @@
+package tfg.k_lendar.views.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tfg.k_lendar.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
