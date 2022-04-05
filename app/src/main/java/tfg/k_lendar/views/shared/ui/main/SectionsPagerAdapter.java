65c53876e6417fd79dc84464a01f45b40dd5c027
davidortegafarrerons <atreidesd32@gmail.com>
2022-04-05 19:55:01 +0200

    Create tabbed activities

---
+++ app/src/main/java/tfg/k_lendar/views/shared/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,50 @@
+package tfg.k_lendar.views.shared.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import tfg.k_lendar.R;
+import tfg.k_lendar.views.shared.ui.main.fragments.NewTaskFragment;
+import tfg.k_lendar.views.shared.ui.main.fragments.NewTruancyFragment;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        fragment = new NewTaskFragment();
+        if (position == 1) {
+            fragment = new NewTruancyFragment();
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/tfg/k_lendar/views/shared/ui/main/fragments/NewTruancyFragment.java
@@ -0,0 +1,20 @@
+package tfg.k_lendar.views.shared.ui.main.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import tfg.k_lendar.R;
+
+public class NewTruancyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_truancy, container, false);
+    }
+}
