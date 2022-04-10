be6ceacf764936bfa2f7d52a2a528c71f89329f3
davidortegafarrerons <atreidesd32@gmail.com>
2022-04-10 20:33:59 +0200

    Create Edit Task & Edit Truancy views

---
+++ app/src/main/java/tfg/k_lendar/views/task/EditTaskActivity.java
@@ -0,0 +1,16 @@
+package tfg.k_lendar.views.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tfg.k_lendar.R;
+
+public class EditTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+    }
+}
