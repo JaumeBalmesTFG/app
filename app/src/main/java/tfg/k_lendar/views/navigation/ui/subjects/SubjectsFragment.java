b28257a1f623b1b652adda45d3cd86fb7fcade21
Maik <mcollb20@jaumebalmes.net>
2022-05-03 20:48:40 +0200

    [F-4] [WIP] - Subject_Fragments modal: add icons, text, floating action button.

--- app/src/main/java/tfg/k_lendar/views/navigation/ui/subjects/SubjectsFragment.java
+++ app/src/main/java/tfg/k_lendar/views/navigation/ui/subjects/SubjectsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import tfg.k_lendar.databinding.SubjectsFragmentBinding;
 
@@ -21,6 +25,7 @@ public class SubjectsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         subjectsViewModel =
                 new ViewModelProvider(this).get(SubjectsViewModel.class);
 
