b3255126a7eeaed3601d0f8df52476b693c0fb05
Andrey <ashevche20@jaumebalmes.net>
2022-05-03 17:17:41 +0200

    [F-2] [WIP] Finished authService

--- app/src/main/java/tfg/k_lendar/views/auth/AuthActivity.java
+++ app/src/main/java/tfg/k_lendar/views/auth/AuthActivity.java
@@ -2,7 +2,6 @@ package tfg.k_lendar.views.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -10,16 +9,22 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.regex.Pattern;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import tfg.k_lendar.R;
+import tfg.k_lendar.http.api.requests.auth.AuthRequest;
+import tfg.k_lendar.http.api.services.auth.AuthPlaceHolderApi;
+import tfg.k_lendar.http.models.auth.Auth;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -56,7 +61,8 @@ public class AuthActivity extends AppCompatActivity {
                     //emailInput.setError("Email empty or not valid");
                     return;
                 }
-                showLoginForm();
+                authService(String.valueOf(emailInput.getText()));
+                //showLoginForm();
                 //Send to auth function
                 Log.d("email", String.valueOf(emailInput.getText()));
 
@@ -169,4 +175,38 @@ public class AuthActivity extends AppCompatActivity {
 
         });
     }
+
+    public void authService(String email){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.klendar.es/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AuthPlaceHolderApi authPlaceHolderApi = retrofit.create(AuthPlaceHolderApi.class);
+
+        AuthRequest authRequest = new AuthRequest(email);
+
+        Call<Auth> call = authPlaceHolderApi.createPost(authRequest);
+
+        call.enqueue(new Callback<Auth>() {
+            @Override
+            public void onResponse(Call<Auth> call, Response<Auth> response) {
+               /* if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+ response.code());
+                    return;
+                }*/
+
+                Auth auth = response.body();
+
+                Log.d("AQUI", auth.getMessage());
+            }
+
+            @Override
+            public void onFailure(Call<Auth> call, Throwable t) {
+                //textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
 }
