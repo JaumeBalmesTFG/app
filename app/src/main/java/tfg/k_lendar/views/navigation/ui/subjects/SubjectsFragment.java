5ac541d9a114e284bb6f78c98560675ce2812742
Maik <mcollb20@jaumebalmes.net>
2022-05-04 17:25:24 +0200

    [F-4] - Subject_Fragments modal: design finished.

--- app/src/main/java/tfg/k_lendar/views/navigation/ui/subjects/SubjectsFragment.java
+++ app/src/main/java/tfg/k_lendar/views/navigation/ui/subjects/SubjectsFragment.java
@@ -12,10 +12,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
+import tfg.k_lendar.R;
 import tfg.k_lendar.databinding.SubjectsFragmentBinding;
 
 public class SubjectsFragment extends Fragment {
@@ -23,8 +22,25 @@ public class SubjectsFragment extends Fragment {
     private SubjectsViewModel subjectsViewModel;
     private SubjectsFragmentBinding binding;
 
+    Button points, buttonEdit;
+    FloatingActionsMenu fabMenu;
+    //FloatingActionButton add, edit, archive;
+    View view;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.subjects_fragment, container, false);
+        points = view.findViewById(R.id.buttonPoints);
+        fabMenu = view.findViewById(R.id.menuFab);
+        buttonEdit = view.findViewById(R.id.buttonEdit);
+
+        buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fabMenu.performClick();
+            }
+        });
 
         subjectsViewModel =
                 new ViewModelProvider(this).get(SubjectsViewModel.class);
