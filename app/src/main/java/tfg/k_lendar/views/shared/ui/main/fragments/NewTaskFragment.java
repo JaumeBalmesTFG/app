fea3258d372a8a6b692347059d903fe344886b9a
davidortegafarrerons <atreidesd32@gmail.com>
2022-05-07 17:20:16 +0200

    [F-6] Created a list and created an adapter for dropdown menu

--- app/src/main/java/tfg/k_lendar/views/shared/ui/main/fragments/NewTaskFragment.java
+++ app/src/main/java/tfg/k_lendar/views/shared/ui/main/fragments/NewTaskFragment.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.Arrays;
+import java.util.List;
 
 import tfg.k_lendar.R;
 
@@ -17,4 +23,14 @@ public class NewTaskFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_new_task, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        AutoCompleteTextView subject = view.findViewById(R.id.subjectsDropdown);
+        String[] items = {"item 1", "item 2", "item 3"};
+        ArrayAdapter adapter = new ArrayAdapter<>(this.getContext(), R.layout.dropdown_item, items);
+        subject.setAdapter(adapter);
+    }
+
+
 }
