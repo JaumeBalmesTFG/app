de3286e76f01c6b5fd24e825f81ca011f4207ec4
davidortegafarrerons <atreidesd32@gmail.com>
2022-05-11 17:34:16 +0200

    [F-21] Added auth bearer

--- app/src/main/java/tfg/k_lendar/core/sharedpreferences/AuthBearerToken.java
+++ app/src/main/java/tfg/k_lendar/core/sharedpreferences/AuthBearerToken.java
@@ -8,6 +8,6 @@ public class AuthBearerToken {
      public static String getAuthBearerToken(Context context) {
          //SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
          //String token = sharedPref.getString("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtsZW5kYXJAZ21haWwuY29tIiwiX2lkIjoiNjI3YTc4YjU0M2U5NjQ1MmUxZmNkYmMxIiwiaWF0IjoxNjUyMTkzNDYxfQ.72d4aELj4YrCIEZdDDp0WntH_VbP_5k3qSFUr0u7ZIk");
-         return "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtsZW5kYXJAZ21haWwuY29tIiwiX2lkIjoiNjI3YTc4YjU0M2U5NjQ1MmUxZmNkYmMxIiwiaWF0IjoxNjUyMTkzNDYxfQ.72d4aELj4YrCIEZdDDp0WntH_VbP_5k3qSFUr0u7ZIk";
+         return "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtsZW5kYXJAZ21haWwuY29tIiwiX2lkIjoiNjI3YmQxYzI0MTY5OTc3Nzk1Mjg2Y2NmIiwiaWF0IjoxNjUyMjgxNzk0fQ.46sPPV7TzBIbaFv4HD1FyUlQh-zmibQWvk6GGEcKK4E";
      }
 }
