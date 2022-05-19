2bd268a017b0df2fc98aa642a0176764cd00d69b
Maik <mcollb20@jaumebalmes.net>
2022-05-19 19:21:21 +0200

    [F-31] - In NewUf: validations for Title, Total Hours and Truancy Percentatge

--- app/src/main/java/tfg/k_lendar/views/uf/NewUfActivity.java
+++ app/src/main/java/tfg/k_lendar/views/uf/NewUfActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import tfg.k_lendar.R;
+import tfg.k_lendar.core.helpers.RemoveErrorTextWatcher;
+import tfg.k_lendar.http.models.taskTruency.PostTaskRequest;
 import tfg.k_lendar.views.navigation.NavigationActivity;
+import tfg.k_lendar.views.shared.TaskTruancyActivity;
 
 public class NewUfActivity extends AppCompatActivity {
 
     Button saveButton, cancelButton, cancelButtonExams, editButtonExams, cancelButtonActivities, editButtonActivities;
     TextInputEditText titleInput, nameInput, percentatgeInput, hoursInput, truancyInput;
+    TextInputLayout titleLayout, hoursLayout, truancyLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +32,13 @@ public class NewUfActivity extends AppCompatActivity {
         cancelButton = findViewById(R.id.cancelButtonUf);
         cancelButtonExams = findViewById(R.id.cancelButtonExams);
         editButtonExams = findViewById(R.id.editButtonExams);
+        titleLayout = findViewById(R.id.titleLayoutUf);
         titleInput = findViewById(R.id.titleInputUf);
         nameInput = findViewById(R.id.nameInputUf);
         percentatgeInput = findViewById(R.id.percentatgesInputUf);
+        hoursLayout = findViewById(R.id.hoursLayoutUf);
         hoursInput = findViewById(R.id.hoursInputUf);
+        truancyLayout = findViewById(R.id.truancyLayoutUf);
         truancyInput = findViewById(R.id.truancyInputUf);
         cancelButtonActivities = findViewById(R.id.cancelButtonActivities);
         editButtonActivities = findViewById(R.id.editButtonActivities);
@@ -41,7 +50,36 @@ public class NewUfActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateFormAndSaveTask()) {
+                    System.out.println("culo");
+                }
+            }
+        });
+
+
     }
 
+    private boolean validateFormAndSaveTask() {
+        if (TextUtils.isEmpty(titleInput.getText())) {
+            titleLayout.setError("The title is required");
+            titleInput.addTextChangedListener(new RemoveErrorTextWatcher(titleLayout));
+            return false;
+        }
+        if (TextUtils.isEmpty(hoursInput.getText())) {
+            hoursLayout.setError("The hours is required");
+            hoursInput.addTextChangedListener(new RemoveErrorTextWatcher(hoursLayout));
+            return false;
+        }
+        if (TextUtils.isEmpty(truancyInput.getText())) {
+            truancyLayout.setError("The truancy is required");
+            truancyInput.addTextChangedListener(new RemoveErrorTextWatcher(truancyLayout));
+            return false;
+        }
+        return true;
+    }
 
 }
